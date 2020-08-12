da660c5c765e52f755d3328cffc914386120f225
Adam Hamsik <adam.hamsik@lablabs.io>
2020-08-12 15:59:42 +0200

    Fix documentation from default one.

--- cmd/server.go
+++ cmd/server.go
@@ -38,13 +38,8 @@ var (
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
 	Use:   "server",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Start phpfpm_exporter server",
+	Long:  `Start server with defined parameters.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Infof("Starting server on %v with path %v", listeningAddress, metricsEndpoint)
 
